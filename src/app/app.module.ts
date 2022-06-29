303db8a21ffd6e2645599eb2347fc5287eb7828b
phenom_ayush_dixit <ayush.dixit@phenompeople.com>
2022-06-29 16:26:33 +0530

    build: initial setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule,CommonModule],
+  providers: [],
+  bootstrap: [],
+})
+export class AppModule {
+  constructor(private injector: Injector) {
+    const el = createCustomElement(AppComponent, { injector });
+    customElements.define('chatbot-management-app', el);
+  }
+  ngDoBootstrap() {}
+}
