9a9cee80121addbb06b535c96e2636770b800a80
Meghana Pechetti <meghana.pechetti@phenompeople.com>
2022-07-06 11:49:16 +0530

    style: removed spaces and comments

--- src/app/lib/checkbox/checkbox.component.ts
+++ src/app/lib/checkbox/checkbox.component.ts
@@ -1,12 +1,4 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  AfterViewInit,
-  ElementRef,
-  ViewChild,
-} from '@angular/core';
+import { Component, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AppEventType } from '../../shared/enums/event.enum';
 import { BroadcastService } from '../../shared/services/broadcast.service';
@@ -16,7 +8,7 @@ import { BroadcastService } from '../../shared/services/broadcast.service';
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss'],
 })
-export class CheckboxComponent implements OnInit, AfterViewInit {
+export class CheckboxComponent implements OnInit {
   @ViewChild('svg') svg: ElementRef | undefined;
 
   constructor(private broadcastService: BroadcastService) {}
@@ -25,8 +17,6 @@ export class CheckboxComponent implements OnInit, AfterViewInit {
 
   @Input('isActive') public isActive!: boolean;
   @Input('isDisabled') public isDisabled!: boolean;
-
-  // @Output('checkboxState') checkBoxState = new EventEmitter();
 
   ngOnInit(): void {
     console.log(this.isActive);
@@ -38,11 +28,6 @@ export class CheckboxComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     if (this.isDisabled) {
-      //  let ele = document.getElementById("fil-this");
-      //  ele?.setAttribute("fill","#8D949A");
-      //  ele?.setAttribute("stroke","#8D949A");
-
-      // console.log(this.svg)
       this.svg?.nativeElement.setAttribute('fill', '#8D949A');
       this.svg?.nativeElement.setAttribute('stroke', '#8D949A');
     }
@@ -50,7 +35,6 @@ export class CheckboxComponent implements OnInit, AfterViewInit {
 
   onChange(event: any) {
     this.isActive = event.target.checked;
-    // this.checkBoxState.emit({state: this.isActive});
     this.broadcastService.broadcast({
       name: AppEventType.CHECKBOX_EVENT,
       data: { isActive: this.isActive },
