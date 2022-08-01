2ef8d3d727303203c53de9a6ae305b5723ceb69d
phenom_ayush_dixit <ayush.dixit@phenompeople.com>
2022-08-01 13:58:00 +0530

    feat: button component created

---
+++ src/app/lib/button/button.component.ts
@@ -0,0 +1,30 @@
+// how to use it
+//  <app-button [label]="'Click Me!'" [icon]="'https://d7pkvxpsevxsc.cloudfront.net/bot-icons/icon_2.svg'" [buttonClass]="'btn-medium'"></app-button>
+
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent implements OnInit {
+  @Input('label') public label!: string;
+  @Input('icon') public icon!: string;
+  @Input('iconPosotion') public iconPosition!: string;
+  @Input('buttonClass') public buttonClass!: string;
+
+  @Output('onClick') onClick = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onButtonClick() {
+    this.onClick.emit({
+      data: {
+        label: this.label,
+      },
+    });
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { ToggleComponent } from '../lib/toggle/toggle.component';
-import { CheckboxComponent } from '../lib/checkbox/checkbox.component';
-import { DropdownComponent } from '../lib/dropdown/dropdown.component';
-import { AccordionComponent } from '../lib/accordion/accordion.component';
+import { ToggleComponent } from 'src/app/lib/toggle/toggle.component';
+import { CheckboxComponent } from 'src/app/lib/checkbox/checkbox.component';
+import { DropdownComponent } from 'src/app/lib/dropdown/dropdown.component';
+import { AccordionComponent } from 'src/app/lib/accordion/accordion.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ProgressbarComponent } from '../lib/progressbar/progressbar.component';
-import { CardComponent } from '../lib/card/card.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LoaderComponent } from './components/loader/loader.component';
+import { ProgressbarComponent } from 'src/app/lib/progressbar/progressbar.component';
+import { CardComponent } from 'src/app/lib/card/card.component';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+import { LoaderComponent } from 'src/app/shared/components/loader/loader.component';
+import { ButtonComponent } from 'src/app/lib/button/button.component';
 
 @NgModule({
   imports: [
@@ -30,6 +31,7 @@ import { LoaderComponent } from './components/loader/loader.component';
     CardComponent,
     HeaderComponent,
     LoaderComponent,
+    ButtonComponent,
   ],
   exports: [
     ProgressbarComponent,
@@ -41,6 +43,7 @@ import { LoaderComponent } from './components/loader/loader.component';
     CardComponent,
     HeaderComponent,
     LoaderComponent,
+    ButtonComponent,
   ],
 })
 export class SharedModule {}
